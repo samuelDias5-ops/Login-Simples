8123bd422793b75f32f7b34bcb68cc67ac13ac00
Samuel Dias <samuelsassenda5@gmail.com>
2026-08-21 20:26:07 +0100

    versão 5(melhorando json e add o gerar_id)

--- dados/cadastro.py
+++ dados/cadastro.py
@@ -1,6 +1,12 @@
-def cadastrar():
+def gerar_id(dados):
+    if not dados:
+        return 1
+    return max(d["id"] for d in dados) + 1
+
+
+
+def cadastrar(id_usuario):
     n = input("Insira seu nome: ")
-    id = 1
     while True:
         try:  
             i = int(input("Insira sua idade: "))
@@ -8,16 +14,14 @@ def cadastrar():
             print("Erro. Insira uma idade válida!")
             continue
         else:
-            print("Novo Cadastro: ", end="")
-
+            print(f"Novo Cadastro: {n} cadastrado(a) com sucesso!")
+    
         dados = {
-            "id": id,
+            "id": id_usuario,
             "nome": n,
             "idade": i
         }
-        for i in dados:
-            i["id"]+=1
-        print(f"{n} cadastrado(a) com sucesso!")
-        
+       
+
         return dados
                     
--- main.py
+++ main.py
@@ -1,11 +1,9 @@
-from dados.cadastro import cadastrar
+from dados.cadastro import cadastrar, gerar_id
 from dados.dados import ler_usuarios, salvar_dados 
-
 
 def menu_inicial():
     while True:
         print("Sistema de Login")
-
         print("1- Cadastar Usuário")
         print("2- Ver Usuários cadastrados")
         print("3- Excluir Usuário")
@@ -16,10 +14,18 @@ def menu_inicial():
 
             match op:
                 case 1:
-                    user = cadastrar()
-                    salvar_dados(user)
+                    user = ler_usuarios()
+                    id_usuario = gerar_id(user)
+                    dado = cadastrar(id_usuario)
+                    salvar_dados(dado)
                 case 2:
-                    ler_usuarios()  
+                    user = ler_usuarios()
+                    for d in user:
+                        print(
+                                f"ID: {d["id"]} |"
+                                f"Nome:\t{d["nome"]} |"
+                                f"Idade:\t{d["idade"]}"
+                            ) 
                 case 3:
                     print("oi") 
                 case 4:
@@ -29,8 +35,5 @@ def menu_inicial():
                     print("Opção não existe!")
         except(ValueError,TypeError, KeyboardInterrupt):
             continue
-
-
-
 menu_inicial()
                 
